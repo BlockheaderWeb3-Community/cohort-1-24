2d54a49e5dc829f9c4bc85c8ad4a9cd81bc5c924
sprtd <sagesdn@gmail.com>
2024-03-21 10:38:20 +0100

    feat: implement inheritance in SimpleCounter

--- contracts/SimpleCounter.sol
+++ contracts/SimpleCounter.sol
@@ -1,12 +1,26 @@
+// trustless - no need for 3rd parties
+// decentralized
+// distributed
+// permissionless
+// secure
+// immutable
+
+
+
+
+
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.8.2 <0.9.0;
+import { Ownable } from "./Ownable.sol";
 
 /**
  * @title SimpleCounter
  * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract SimpleCounter {
+contract SimpleCounter is Ownable {
 
     uint256 count;
 
@@ -22,18 +36,18 @@ contract SimpleCounter {
 
     /**
      * @dev Return value 
-     * @return value of count
+     * @return value of 'number'
      */
-    function retrieve() public view returns (uint256){
+    function retrieve() public view returns(uint256) {
         return count;
     }
 
 
-    function increaseCount() public {
+    function increaseCount() public onlyOwner() {
         count += 1;
     }
 
-    function decreaseCount() public {
+    function decreaseCount() public  {
         count -= 1;
     }
 
@@ -55,4 +69,17 @@ contract SimpleCounter {
     function getUnderCount() public view returns (int256){
         return underCount;
     }
+
+    function isOwner() public view returns(bool) {
+       address currentOwner = getCurrentOwner();
+       if(currentOwner == owner) return true;
+       return false;
+    }
+
+    function whoIsOwner() public view returns (address) {
+            return getCurrentOwner();
+    }
+
+
+    
 }

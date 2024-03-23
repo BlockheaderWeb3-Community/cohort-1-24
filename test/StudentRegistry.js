1e8807affd73f053452c44762b4797510af28f03
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-03-23 11:11:10 +0100

    feat: implement delete test

--- test/StudentRegistry.js
+++ test/StudentRegistry.js
@@ -22,6 +22,11 @@ describe("StudentRegistry", function () {
 		);
 	});
 
+	it("Should retreive total number of students from student ", async function () {
+		const students = await studentRegistry.studentsCounter();
+		expect(students).to.equal(2); 
+		
+	});
 
 	it("Should retreive student by address and studentId ", async function () {
 		const students = await studentRegistry.getStudentDetails("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", 1);
@@ -30,16 +35,11 @@ describe("StudentRegistry", function () {
 	});
 
 
-	it("Should retreive total number of students from student ", async function () {
-		const students = await studentRegistry.studentsCounter();
-		expect(students).to.equal(2); 
-		
-	});
 
-	it("Should retreive student by studentAddress and studentId ", async function () {
-		const students = await studentRegistry.getStudentDetails("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", 1);
-		expect(students.name).to.equal("Crypto");
-		expect(students.age).to.equal(20);
+	it("Should delete student by studentAddress and studentId ", async function () {
+		const students = await studentRegistry.deleteStudent("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", 1);
+		expect(students.name).to.equal(undefined);
+		expect(students.age).to.equal(undefined);
 	});
 
 

8494c20b76c76bf632b89ff9da817759cb3d9fa6
sprtd <sagesdn@gmail.com>
2024-04-15 11:23:26 +0100

    test: store transaction

--- test/JustCounter.js
+++ test/JustCounter.js
@@ -10,11 +10,30 @@ describe("JustCounter Test Suite", function () {
         return { JustCounter, owner, addr1, addr2 };
     }
 
-    describe("Post Deployment State", async () => {
+    describe("Post Deployment State Variables", async () => {
         it("Should return state variables", async () => {
             const { JustCounter } = await loadFixture(deployTokenFixture);
             expect(await JustCounter.count()).to.equal(0);
             expect(await JustCounter.underCount()).to.equal(0);
+        })
+    })
+
+    describe.only("State Variables Changes", async () => {
+        it("Should store number", async () => {
+            let amount = 5
+            // get loadFixture variables
+            const { JustCounter } = await loadFixture(deployTokenFixture);
+            // get current state variable count
+            let count1 = await JustCounter.count()
+            console.log("count before state change___", count1)
+            // write assertion statement for count1
+            expect(count1).to.equal(0);
+
+            // store count (transaction)
+            await JustCounter.store(amount)
+            let count2 = await JustCounter.count()
+            // write assertion statement for count after store txn
+            expect(count2).to.equal(amount);
         })
     })
 

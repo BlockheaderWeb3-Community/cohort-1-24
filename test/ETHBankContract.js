57ab91b40183cf786be52a7a1142e168218c10dc
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-04-20 10:06:52 +0100

    test: should withdraw ETH

--- test/ETHBankContract.js
+++ test/ETHBankContract.js
@@ -22,18 +22,13 @@ describe("ETHBank Test Suite", function () {
   describe("Transactions", async () => {
     it("Should deposit ETH", async () => {
       const { ETHBankContract, addr1 } = await loadFixture(deployTokenFixture);
-
       // Get the balance of addr1 before the deposit
-      const beforeBalance = await ETHBankContract.ethBalances(addr1.address);
-
+      const balanceBefore = await ETHBankContract.ethBalances(addr1.address);
       // assert that the sender's balance is 0 before the deposit
-      expect(beforeBalance).to.eq(0);
-
+      expect(balanceBefore).to.eq(0);
       const depositAmount = ethers.utils.parseEther("1");
-
       // assert that the deposit amount is not 0
       expect(depositAmount).not.to.eq(0);
-
       // Deposit the ETH
       const transaction = await ETHBankContract.connect(addr1).depositETH({ value: depositAmount });
       const receipt = await transaction.wait();
@@ -42,15 +37,50 @@ describe("ETHBank Test Suite", function () {
       expect(receipt).to.exist;
 
       // Get the balance of addr1 after the deposit
-      const newBalance = await ETHBankContract.ethBalances(addr1.address);
+      const balanceAfter = await ETHBankContract.ethBalances(addr1.address);
 
       // assert that the new balance of addr1 is equal to depositAmount
-      expect(newBalance).to.eq(depositAmount);
+      expect(balanceAfter).to.eq(depositAmount);
 
       // assert that the Deposit event was emitted with the correct arguments
       expect(receipt.events[0])
         .to.emit(ETHBankContract, "Deposit")
-        .withArgs(addr1.address, depositAmount, beforeBalance, newBalance);
+        .withArgs(addr1.address, depositAmount, balanceBefore, balanceAfter);
     });
   });
+
+  it("Should withdraw ETH", async () => {
+    const { ETHBankContract, addr1 } = await loadFixture(deployTokenFixture);
+    const depositAmount = ethers.utils.parseEther("1");
+
+    // Deposit some ETH to the contract
+    await ETHBankContract.connect(addr1).depositETH({ value: depositAmount });
+
+    // Get the balance of addr1 before the withdrawal
+    const balanceBefore = await ETHBankContract.ethBalances(addr1.address);
+
+    // Assert that addr1 balance is not 0
+    expect(balanceBefore).not.to.eq(0);
+
+    const withdrawalAmount = ethers.utils.parseEther("0.5");
+
+    // Assert that withdraw amount is not greater than balance
+    expect(balanceBefore).to.be.at.least(withdrawalAmount);
+
+    // Withdraw ETH from the contract
+    const transaction = await ETHBankContract.connect(addr1).withdrawETH(withdrawalAmount);
+    const receipt = await transaction.wait();
+
+    // Get the balance of addr1 after the withdrawal
+    const balanceAfter = await ETHBankContract.ethBalances(addr1.address);
+
+    // Assert that the balance of addr1 is correctly updated after the withdrawal
+    expect(balanceAfter).to.eq(balanceBefore.sub(withdrawalAmount));
+
+    // Assert that the Withdraw event was emitted with the correct arguments
+    expect(receipt.events[0])
+        .to.emit(ETHBankContract, "Withdraw")
+        .withArgs(addr1.address, withdrawalAmount, balanceBefore, balanceAfter);
+});
+
 });

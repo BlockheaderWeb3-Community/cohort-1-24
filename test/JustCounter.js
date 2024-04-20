09c1c62fd48c8b10a7e44825200e394c317e5283
Asher <theforce289@gmail.com>
2024-04-20 12:05:49 +0100

    test(ETHBankContract): validate ETH Withdrawal

--- test/JustCounter.js
+++ test/JustCounter.js
@@ -1,19 +1,14 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
+const { deployTokenFixture } = require("../utils/index.js");
 
 describe("JustCounter Test Suite", function () {
   // define loadFixture
   // fixtures can return anything you consider useful for your tests
-  const deployTokenFixture = async () => {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const JustCounter = await ethers.deployContract("JustCounter");
-    return { JustCounter, owner, addr1, addr2 };
-  };
-
   describe("Post Deployment State Variables", async () => {
     it("Should return state variables", async () => {
       // get loadFixture variables
-      const { JustCounter } = await loadFixture(deployTokenFixture);
+      const { JustCounter } = await loadFixture(deployTokenFixture("JustCounter"));
       expect(await JustCounter.count()).to.equal(0);
       expect(await JustCounter.underCount()).to.equal(0);
     });
